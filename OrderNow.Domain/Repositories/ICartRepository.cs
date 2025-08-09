13da74ef73c3e2bba05d43aa37adc959b70776fd
Mizan-ICE <mizan596644@gmail.com>
2025-08-09 10:39:10 +0600

    update

---
+++ OrderNow.Domain/Repositories/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNow.Domain.Repositories;
+internal interface ICartRepository
+{
+}
