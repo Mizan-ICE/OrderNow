76a7afcd1c24c542cb6c832c62546e88854ae3ac
Mizan-ICE <mizan596644@gmail.com>
2025-08-11 15:37:14 +0600

    components

---
+++ OrderNow.Web/Models/ViewComponents/CartSummary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderNow.Domain.Services;
+
+namespace OrderNow.Web.Models.ViewComponents;
+
+public class CartSummary : ViewComponent
+{
+    public ICartService _cartService;
+    public CartSummary(ICartService cartService)
+    {
+        _cartService = cartService;
+
+    }
+    public IViewComponentResult Invoke()
+    {
+        var items = _cartService.GetCartItems();
+        //var totalPrice = _cartService.GetCartTotalPrice();
+        return View(items.Count);
+    }
+}
